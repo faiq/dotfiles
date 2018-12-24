7f69349f7102ae83d091bc019d1317e11678efc1
faiq <faiqrazarizvi@gmail.com>
2018-12-24 12:59:57 -0800

    fix install sh

--- install.sh
+++ install.sh
@@ -13,11 +13,8 @@ do
     *)
       ln -s "$f" "$HOME/.${f##*/}"
   esac
-  #if [ "$f" != "install.sh" ]; then
-  #  ln -s "$f" "$HOME/.${f##*/}"
-  #fi
 done
-#mkdir -p ~/.vim/bundle
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#vim +PluginInstall +qall
-#source ~/.bash_profile
+mkdir -p ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+source ~/.bash_profile
