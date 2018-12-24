069bec4f71aaa6e3d825672f24af01633aa328a9
faiq <faiqrazarizvi@gmail.com>
2018-12-24 12:57:55 -0800

    linux goodness

--- install.sh
+++ install.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-set -x
-[ "$UID" -eq 0 ] || { echo "This script must be run as root."; exit 1;}
+#set -x
+#[ "$UID" -eq 0 ] || { echo "This script must be run as root."; exit 1;}
 
 for f in ~/dotfiles/*
 do
-  if [ "$f" != `basename "$0"` ]; then
-    ln -s "$f" "$HOME/.${f##*/}"
-  fi
+  case $f in
+    $(pwd)/install.sh)
+      ;;
+    $(pwd)/bin)
+      ln -s "$f" "$HOME/${f##*/}"
+      ;;
+    *)
+      ln -s "$f" "$HOME/.${f##*/}"
+  esac
+  #if [ "$f" != "install.sh" ]; then
+  #  ln -s "$f" "$HOME/.${f##*/}"
+  #fi
 done
-mkdir -p ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
-source ~/.bash_profile
+#mkdir -p ~/.vim/bundle
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#vim +PluginInstall +qall
+#source ~/.bash_profile
